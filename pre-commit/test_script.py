f68bd006da214724be9d3baeeb30c031099ce62c
emrecr9 <68870517+emrecr9@users.noreply.github.com>
2021-11-30 19:35:51 -0500

    update test_script

--- pre-commit/test_script.py
+++ pre-commit/test_script.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+import git
+import pandas as pd
 
 fileContent = subprocess.run("ls", capture_output=True, text=True)
 
@@ -18,19 +21,59 @@ for fileChanged in fileChangedDirectories:
     if str(diffContent.stdout).__contains__("@Test"):
         numTestFiles += 1
 
-def runEvosuite():
-    import os
-    import git
 
-def checkCoverage():
-    import os
+def runEvosuite():
+    print('evosuite')
+
+
+def checkCoverage(classdirectory):
+    coverage = 0.0
+    repository_directory = git.Repo('.', search_parent_directories=True).working_tree_dir
+
+    cmdf = open("runGradle.cmd", "w+")
+    cmdf.write("gradlew build\n")
+    cmdf.close()
+
+    cmd = open("runCoverage.cmd", "w+")
+    cmd.write("cd \"" + ".git\CIS-580-Scripts-main" + "\"\n")
+    cmd.write("java -jar jacococli.jar report --classfiles " + " \"" + repository_directory + classdirectory + "\" --csv \""
+              + repository_directory + "\\coverageReport.csv\"")
+    cmd.close()
+
+    subprocess.call([r'runGradle.cmd'])
+    subprocess.call([r'runCoverage.cmd'])
+
+    if os.path.isfile("runCoverage.cmd"):
+        os.remove("runCoverage.cmd")
+
+        if os.path.isfile("coverageReport.csv"):
+            cf = pd.read_csv(repository_directory + '\\coverageReport.csv')
+            coverageList = cf.BRANCH_COVERED.tolist()
+
+            for c in coverageList:
+                coverage = c + coverage
+
+            if (len(coverageList) > 0):
+                coverage = coverage / len(coverageList)
+            print(coverage, ' ', len(coverageList))
+        else:
+            print("Unable to determine the coverage")
+
+    return coverage
+
+
+if os.path.exists('./src'):
+    path = '\\src'
+else:
+    path = ''
 
 if numTestFiles == 0:
     print("Your changes do not contain any tests\nTests will now be automatically generated")
     runEvosuite()
+    checkCoverage(path)
 else:
     print("The changes have been tested")
 
-checkCoverage()
-
-
+    if checkCoverage(path) < 50:
+        print('The code coverage test is low (below 50)\nTest cases will now be automatically generated')
+        runEvosuite()
